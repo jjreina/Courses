b57a1ee621aaba885c0ad4ee4df2923b881936db
jjreina <jreinapascual@gmail.com>
2018-01-17 16:18:15 +0100

    Third course: - Add Preventing Transitions on About page.

--- Pluralsight/3. Reactjs-Building Applications with React and Flux/src/components/about/aboutPage.tsx
+++ Pluralsight/3. Reactjs-Building Applications with React and Flux/src/components/about/aboutPage.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { Prompt } from 'react-router-dom';
 
 export const About: React.StatelessComponent<{}> = () => {
     return (
-        <div>
+            <div>
+            <Prompt message="Are you sure you want to leave About page?" />
             <h1>About</h1>
             <p>
                 This application uses the following technologies:
