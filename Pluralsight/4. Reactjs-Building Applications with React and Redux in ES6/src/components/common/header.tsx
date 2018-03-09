f8a00733a52d51aa0369a75a40bd3c2cd37329a1
EJUAREI <juan.jesus.reina.pascual.de.lallana@ericsson.com>
2018-03-09 16:28:03 +0100

    React fourth course - Added new Loading dots componet.

--- Pluralsight/4. Reactjs-Building Applications with React and Redux in ES6/src/components/common/header.tsx
+++ Pluralsight/4. Reactjs-Building Applications with React and Redux in ES6/src/components/common/header.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { LoadingDots } from './loadingDots';
+
 export const Header: React.StatelessComponent = () => {
     const activeStyle = { color: 'orange' };
     return(
@@ -10,6 +12,7 @@ export const Header: React.StatelessComponent = () => {
             <NavLink to="/courses" activeStyle={activeStyle}>Courses</NavLink>
             {' | '}
             <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+            <LoadingDots interval={100} dots={20}/>
         </nav>
     );
 };
