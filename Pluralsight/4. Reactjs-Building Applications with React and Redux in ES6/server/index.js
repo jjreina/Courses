6761efb098cdf3bc4a1ef1cce0cd26c2a4b08aa2
jjreina <jreinapascual@gmail.com>
2018-03-16 15:59:05 +0100

    React fourth course: - Added compression on production requests server.

--- Pluralsight/4. Reactjs-Building Applications with React and Redux in ES6/server/index.js
+++ Pluralsight/4. Reactjs-Building Applications with React and Redux in ES6/server/index.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let path = require('path');
+let compression = require('compression');
 
 const app = express();
 const publicPath = path.resolve(__dirname, '../dist');
 
+app.use(compression());
 app.use(express.static(publicPath));
 
 app.listen(process.env.port || 3000, () =>
