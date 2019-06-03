d1ebb3c8c17087168901c0b9a49011f64e5bce52
jjreina <jreinapascual@gmail.com>
2019-06-03 23:06:49 +0200

    Android:EditText + Button

---
+++ Udemy/2. The Complete Android Oreo Developer Course/Section 2/LoginDemo/app/src/main/java/juan/reina/logindemo/MainActivity.java
@@ -0,0 +1,25 @@
+package juan.reina.logindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View v) {
+        EditText username = findViewById(R.id.userNameEditText);
+        EditText password = findViewById(R.id.passwordEditText);
+
+        Log.i("Info", "Button pressed!!");
+        Log.i("Username", username.getText().toString());
+        Log.i("Password", password.getText().toString());
+    }
+}
