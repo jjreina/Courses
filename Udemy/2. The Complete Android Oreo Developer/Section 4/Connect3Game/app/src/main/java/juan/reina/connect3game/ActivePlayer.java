80947b85a8e54d03f68a014188e7da3791085e66
jjreina <jreinapascual@gmail.com>
2019-07-31 12:39:48 +0200

    Android: Connect 3 game
    - Alpha version

--- Udemy/2. The Complete Android Oreo Developer/Section 4/Connect3Game/app/src/main/java/juan/reina/connect3game/ActivePlayer.java
+++ Udemy/2. The Complete Android Oreo Developer/Section 4/Connect3Game/app/src/main/java/juan/reina/connect3game/ActivePlayer.java
@@ -9,11 +9,13 @@ public class ActivePlayer {
 
     @Retention(RetentionPolicy.SOURCE)
     @StringDef({
+            EMPTY,
             YELLOW,
             RED
     })
     public @interface ActivePlayerMode {}
 
+    public static final String EMPTY = "empty";
     public static final String YELLOW = "yellow";
     public static final String RED = "red";
 
--- Udemy/2. The Complete Android Oreo Developer/Section 4/Connect3Game/app/src/main/java/juan/reina/connect3game/MainActivity.java
+++ Udemy/2. The Complete Android Oreo Developer/Section 4/Connect3Game/app/src/main/java/juan/reina/connect3game/MainActivity.java
@@ -6,10 +6,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+import static juan.reina.connect3game.ActivePlayer.*;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] gameState = new String[]{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY};
+    int[][] winnerPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                              {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                              {0, 4, 8}, {2, 4, 6}};
     ActivePlayer activePlayer = new ActivePlayer();
 
     @Override
@@ -19,18 +28,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void dropIn(View view) {
-        Log.i("Info", "Tapped");
+
         ImageView counter = (ImageView) view;
+        // Log.i("Info", "Tapped image: " + counter.getTag().toString());
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        gameState[tappedCounter] = activePlayer.getActivePlayerMode();
+
         counter.setTranslationY(-1500);
 
-        if (activePlayer.getActivePlayerMode() == ActivePlayer.YELLOW) {
+        if (activePlayer.getActivePlayerMode() == YELLOW) {
             counter.setImageResource(R.drawable.yellow);
             counter.animate().translationYBy(1500).rotation(3600).setDuration(1000);
-            activePlayer.setActivePlayerMode(ActivePlayer.RED);
+            activePlayer.setActivePlayerMode(RED);
         } else {
             counter.setImageResource(R.drawable.red);
             counter.animate().translationYBy(1500).rotation(3600).setDuration(1000);
-            activePlayer.setActivePlayerMode(ActivePlayer.YELLOW);
+            activePlayer.setActivePlayerMode(YELLOW);
+        }
+
+        // Refactor to method
+        for (int[]winnerPosition : winnerPositions) {
+            if (gameState[winnerPosition[0]] == gameState[winnerPosition[1]] && gameState[winnerPosition[1]] == gameState[winnerPosition[2]] && gameState[winnerPosition[0]] != EMPTY) {
+                String winner = activePlayer.getActivePlayerMode() == YELLOW ? RED : YELLOW;
+                Toast.makeText(this, winner + " has won!", Toast.LENGTH_SHORT).show();
+            }
         }
 
     }
