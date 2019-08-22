b45e57e646754212fa7235dde95d13d540107d8c
jjreina <jreinapascual@gmail.com>
2019-08-22 11:35:35 +0200

    Android: Basic audio player
    Start, pause and stop functions implemented.

---
+++ Udemy/2. The Complete Android Oreo Developer/Section 4/SoundDemo/app/src/main/java/juan/reina/sounddemo/MainActivity.java
@@ -0,0 +1,43 @@
+package juan.reina.sounddemo;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.sheeran);
+    }
+
+    public void play(View view) {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.sheeran);
+        }
+        mediaPlayer.start();
+    }
+
+    public void pause(View view) {
+        if (mediaPlayer != null) {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.pause();
+            }
+        }
+    }
+
+    public void stop(View view) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
