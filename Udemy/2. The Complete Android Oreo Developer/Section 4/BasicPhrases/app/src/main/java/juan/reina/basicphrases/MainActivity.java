7e9f1a9c13f1d53729b5f705d899279c49e8ebc6
jjreina <jreinapascual@gmail.com>
2019-09-01 11:57:04 +0200

    Android: Basic layout 4 files x 2 rows, 8 buttons
    - Each button playback one English phrase using Mediaplayer class.

--- Udemy/2. The Complete Android Oreo Developer/Section 4/BasicPhrases/app/src/main/java/juan/reina/basicphrases/MainActivity.java
+++ Udemy/2. The Complete Android Oreo Developer/Section 4/BasicPhrases/app/src/main/java/juan/reina/basicphrases/MainActivity.java
@@ -1,7 +1,11 @@
 package juan.reina.basicphrases;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playPhrase(View view) {
+
+        Button buttonPress = (Button) view;
+        // Log.i("Button pressed", buttonPress.getTag().toString());
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonPress.getTag().toString(), "raw", getPackageName()));
+        mediaPlayer.start();
     }
 }
