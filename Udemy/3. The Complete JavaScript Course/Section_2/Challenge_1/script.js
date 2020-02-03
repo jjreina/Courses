00704848f21599fd2f2e6b4045aa8369eee91fe5
jjreina <jreinapascual@gmail.com>
2020-02-03 14:03:35 +0100

    Javascript: Challenge 1
    Values and basics operations

---
+++ Udemy/3. The Complete JavaScript Course/Section_2/Challenge_1/script.js
@@ -0,0 +1,12 @@
+var massMark = 78;
+var heightMark = 1.69;
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (Math.pow(heightJohn, 2));
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
