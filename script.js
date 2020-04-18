45c5b703351de50bb6d7af732624f215d7aad04b
jjreina <jreinapascual@gmail.com>
2020-04-18 17:50:18 +0200

    Functions expression

--- script.js
+++ script.js
@@ -71,9 +71,27 @@ if (value == 1) {
     console.log('The == equal operator');
 } 
 
-// Functions
+// Functions declaration
 function calculateAge(birthYear) {
     return new Date().getFullYear() - birthYear
 }
 
 console.log(calculateAge(1979));
+
+// Functions expression
+let whatDoYouDo = (job, firstName) => {
+    switch (job) {
+        case 'teacher':
+            return `${firstName} teaches kids to code`;
+        case 'driver':
+            return `${firstName} drives an uber in Lisbon.`;
+        case 'designer': 
+            return `${firstName} designs beautiful websites.`;
+        default:
+            return `${firstName} does something else`;
+    }
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
