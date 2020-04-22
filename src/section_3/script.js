e002ab23674c6bca0389b366d0ea0a510479cfb5
jjreina <jreinapascual@gmail.com>
2020-04-22 17:12:57 +0200

    The 'this' Keyword

--- src/section_3/script.js
+++ src/section_3/script.js
@@ -12,5 +12,31 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(c); // Out of scoping
+    // console.log(c); // Out of scoping
+    console.log(a + d); // Right scope
+    
 }
+
+// The this keyword
+console.log(this);
+
+calculateAge(1979);
+
+function calculateAge(year) {
+    console.log(new Date().getFullYear() - year);
+    console.log(this);
+}
+
+john = {
+    name: 'John',
+    yearOfBirth: 1979,
+    calculateAge: function() {
+        console.log(this);
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+john.calculateAge();
