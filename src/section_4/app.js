7c4fece02899f29c861b3e1b7e2aa4e7a7bfdaa0
jjreina <jreinapascual@gmail.com>
2020-04-25 22:19:55 +0200

    Events and Event Handling: Rolling the Dice

--- src/section_4/app.js
+++ src/section_4/app.js
@@ -7,11 +7,22 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-let scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
-dice = Math.floor(Math.random() * 6) + 1;
 
-document.getElementById('current-' + activePlayer).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+    let  dice = Math.floor(Math.random() * 6) + 1;
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = `img/dice-${dice}.png`;
+});
