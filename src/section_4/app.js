ce640577a26171ae2132230df88fe355e3793188
jjreina <jreinapascual@gmail.com>
2020-04-26 21:03:49 +0200

    Implemented 'Hold' Function and the DRY Principle

--- src/section_4/app.js
+++ src/section_4/app.js
@@ -18,11 +18,23 @@ document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = 0;
 document.getElementById('score-1').textContent = 0;
 document.getElementById('current-0').textContent = 0;
-document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+let diceDOM = document.querySelector('.dice');
+
+function nextPlayer() {
+    document.getElementById(`current-${activePlayer}`).textContent = '0';
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    diceDOM.style.display = 'none';
+}
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
     let  dice = Math.floor(Math.random() * 6) + 1;
-    let diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = `img/dice-${dice}.png`;
 
@@ -30,13 +42,21 @@ document.querySelector('.btn-roll').addEventListener('click', () => {
         roundScore += dice;
         document.getElementById(`current-${activePlayer}`).textContent = roundScore;
     } else {
-        document.getElementById(`current-${activePlayer}`).textContent = '0';
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        diceDOM.style.display = 'none';
+        nextPlayer();
     }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+    scores[activePlayer] += roundScore;
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 20) {
+        document.getElementById(`name-${activePlayer}`).textContent = 'Winner!';
+        diceDOM.style.display = 'none';
+        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+
 });
