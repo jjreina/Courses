9439ce2d36bb2c5a157504996440a71ba2cfb5ad
jjreina <jreinapascual@gmail.com>
2020-05-14 17:04:23 +0200

    Creating Objects: Object.create

--- src/section_5/sandbox.js
+++ src/section_5/sandbox.js
@@ -1,3 +1,4 @@
+//* Function constructor *//
 // JS Contractor
 let Person = function(name, yearOfBirth, job) {
     this.name = name;
@@ -25,3 +26,21 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+
+//* Object create *//
+let personProto = {
+    calculateAge: function() {
+        console.log(new Date().getFullYear() - this.yearOfBirth);       
+    }
+}
+
+let john_2 = Object.create(personProto);
+john_2.name = 'John 2';
+john_2.yearOfBirth = 1979;
+john_2.job = 'Programer';
+
+let jane_2 = Object.create(personProto, {
+    name: { value: 'Jane 2' },
+    yearOfBirth: { value: '1982' },
+    job: { value: 'designer' },
+})
