9748d7c7a7f4e3d00b4fa139949d1d18f0b7c7c7
jjreina <jreinapascual@gmail.com>
2020-05-19 17:22:01 +0200

    Bind, call and apply

--- src/section_5/sandbox.js
+++ src/section_5/sandbox.js
@@ -65,8 +65,6 @@ console.log(obj1.age);
 console.log(obj2.age);
 
 //* Passing function as argument *//
-let years = [1979, 1980, 1981, 1982, 1983, 2015];
-
 calculateAge = (year) => {
     return new Date().getFullYear() - year;
 }
@@ -83,6 +81,7 @@ arrayCalc = (arr, fn) => {
     return arrRes;
 }
 
+let years = [1979, 1980, 1981, 1982, 1983, 2015];
 let ages = arrayCalc(years, calculateAge);
 let fullAges = arrayCalc(ages, isFullAge);
 console.log(ages);
@@ -123,8 +122,66 @@ interviewQuestion('teacher')('Mary');
     console.log(score >= 5 - goodLuck);
 })(5);
 
+//* Closures *//
+retirement = (retirementAge) => {
+    let text = 'years left until retirement.'
+    return (yearOfBirth) => {
+        let age = new Date().getFullYear() - yearOfBirth;
+        console.log(`${retirementAge - age} ${text}`);
+    }
+}
 
+let retirementUS = retirement(66);
+retirementUS(1990);
 
+retirement(65)(1979);
 
+interviewQuestionClosure = (job) => {
+    return (name) => {
+        if (job === 'designer') {
+            let text = ', can you please explain what UX design is';
+            console.log(`${name}${text}`);
+        } else if (job === 'teacher') {
+            let text = 'What subject do you teach,';
+            console.log(`${text} ${name}?`);
+        } else {
+            let text1 = 'Hello';
+            let text2 = ', what do you do?';
+            console.log(`${text1} ${name} ${text2}`);
+        }
+    }    
+}
 
+interviewQuestionClosure('designer')('Juan');
+let teacherQuestionClosure = interviewQuestionClosure('teacher');
+teacherQuestionClosure('Bea');
+
+//* Bind, call and apply *//
+let alejandra = {
+    name: 'Alejandra',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log(`Good ${timeOfDay}, Ladies and gentlemen! I´m ${this.name}, I´m a ${this.job} and I´m ${this.age} years old.`);
+        } else if (style = 'friendly') {
+            console.log(`Hey! What´s up? I´m ${this.name}, I´m a ${this.job} and I´m ${this.age} years old. Have a nice ${timeOfDay}.`);
+            
+        }
+    }
+};
+
+let emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+alejandra.presentation('formal', 'morning');
+alejandra.presentation.call(emily, 'friendly', 'afternoon');
+alejandra.presentation.apply(emily, ['formal', 'evening']);
+
+let johnFriendly = alejandra.presentation.bind(alejandra, 'friendly');
+johnFriendly('morning');
+johnFriendly('night');
 
