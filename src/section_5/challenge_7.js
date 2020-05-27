4baf16687ace07457e48e048ab17319c6d3ea857
jjreina <jreinapascual@gmail.com>
2020-05-27 18:39:21 +0200

    Coding Challenge 7: Solution, Part 2

--- src/section_5/challenge_7.js
+++ src/section_5/challenge_7.js
@@ -12,12 +12,18 @@
     5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
     6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
     7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+--- Expert level ---
+    8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+    9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+    10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+    11. Display the score in the console. Use yet another method for this.
 */
 
 
 // 7. Suppose this code would be a plugin for other programmers to use in their code.
 // So make sure that all your code is private and doesn't interfere with the other programmers code
 // (Hint: we learned a special technique to do exactly that).
+
 (function() {
     // 1. Function constructor
     function Question(question, answer, correctAnswer) {
@@ -44,6 +50,13 @@
             console.log(`${index}. ${answer}`);
         });
     }
+
+     // 11. Display the score in the console. Use yet another method for this.
+     Question.prototype.displayScore = function(score) {
+        console.log(`Your current score is: ${score}`);
+        console.log('-------------------------------');
+        
+    }
     
     let randomQuestion = Math.floor(Math.random() * arrayQuestions.length);
     let question = arrayQuestions[randomQuestion];
@@ -53,13 +66,44 @@
     // The user should input the number of the correct answer such as you displayed it on Task 4.
     let userAnswer = parseInt(prompt('Please select the correct answer.'));
     
+    // 10. Track the user's score to make the game more fun! So each time an answer is correct, 
+    // add 1 point to the score (Hint: I'm going to use the power of closures for this, 
+    // but you don't have to, just do this with the tools you feel more comfortable at this point).
+    function score() {
+        let sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+
+    let keepScore = score();
+
     // 6. Check if the answer is correct and print to the console whether the answer is correct or not
     // (Hint: write another method for this).
-    Question.prototype.checkAnswer = function(userAnswer) {
+    Question.prototype.checkAnswer = function(userAnswer, callback) {
         console.log(this.correctAnswer === userAnswer ? 'Correct answer!' : 'Wrong answer. Try again!');
+        this.displayScore(callback(this.correctAnswer === userAnswer));
     }
-    question.checkAnswer(userAnswer);
-    
+    question.checkAnswer(userAnswer, keepScore);
+
+    // 8. After you display the result, display the next random question, so that the game never ends
+    // (Hint: write a function for this and call it right after displaying the result)
+    nextQuestion = () => {
+        let userAnswer = prompt('Please select the correct answer.');
+        // 9. Be careful: after Task 8, the game literally never ends. 
+        // So include the option to quit the game if the user writes 'exit' instead of the answer. 
+        // In this case, DON'T call the function from task 8.
+        if (userAnswer !== 'exit') {
+            let randomQuestion = Math.floor(Math.random() * arrayQuestions.length);
+            let question = arrayQuestions[randomQuestion];
+            question.displayQuestion();
+            question.checkAnswer(parseInt(userAnswer), keepScore);
+            nextQuestion();
+        }
+    }
+    nextQuestion();
+
 })();
-
-
