16fe21d0f919c79d3cf9a5aa1248d53dee0f25af
jjreina <jreinapascual@gmail.com>
2020-06-05 18:45:37 +0200

    Reading Input Data

--- src/section_6/app.js
+++ src/section_6/app.js
@@ -4,20 +4,43 @@ let budgetController = (() => {
 
 let uiController = (() => {
 
+    const DOM_STRINGS = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    return {
+        getInput: () => {
+            return {
+                type: document.querySelector(DOM_STRINGS.inputType).value, // inc or exp.
+                description: document.querySelector(DOM_STRINGS.inputDescription).value, 
+                value: document.querySelector(DOM_STRINGS.inputValue).value
+            }
+        },
+
+        getDOMStrings: () => {
+            return DOM_STRINGS;
+        }
+    }
+
 })();
 
 let controller = ((budgetCtrl, uiCtrl) => {
 
     const ENTER_KEYCODE = 13;
+    const DOM = uiCtrl.getDOMStrings();
 
     let ctrlAddItem = () => {
-        console.log('It´s work');
+        // Get the field input  data
+        let input = uiCtrl.getInput();
+        console.log(input);
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', (event) => {
-
+        
         if (event.keyCode === ENTER_KEYCODE || event.which === ENTER_KEYCODE) {
             ctrlAddItem();
         }
