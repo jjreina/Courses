cc03e124fb4eeea0e31205bb6117b251bda4595f
jjreina <jreinapascual@gmail.com>
2020-07-27 17:31:01 +0200

    Firebase Cloud Functions + REST + Express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,20 @@
 import * as functions from 'firebase-functions';
+import {db} from './init';
+import express from 'express';
+import cors from 'cors';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-   response.status(200).json({message: 'Hello World'});
+app.use(cors({origin: true}));
+
+app.get('/courses', async (request, response) => {
+  const snaps = await db.collection('courses').get();
+  const courses: any[] = [];
+
+  snaps.forEach((snap: { data: () => any; }) => courses.push(snap.data()));
+
+  response.status(200).json({courses});
 });
+
+export const getCourses = functions.https.onRequest(app);
+
