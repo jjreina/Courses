f42692d0dcb2aa194a68640c85c81fe2016e262e
jjreina <jreinapascual@gmail.com>
2022-12-29 15:34:10 +0100

    Mockdata

--- fazt-react-begginer/src/mockData/tasks.js
+++ fazt-react-begginer/src/mockData/tasks.js
@@ -2,16 +2,16 @@ export const tasks = [
   {
     id: 0,
     title: "task one",
-    description: "task one",
+    description: "task one description",
   },
   {
     id: 1,
     title: "task two",
-    description: "task two",
+    description: "task two description",
   },
   {
     id: 2,
     title: "task three",
-    description: "task three",
+    description: "task three description",
   },
 ];
