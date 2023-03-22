e2b13875635c466381f4ca29f9c9bd31c7154e69
jjreina <jreinapascual@gmail.com>
2023-03-22 19:56:33 +0100

    Hello World Reack

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <>My wishlist with modern React</>
+  </React.StrictMode>
+);
