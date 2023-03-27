c28e731d7ab37e7d9932c6b1caac28878c41bb28
jjreina <jreinapascual@gmail.com>
2023-03-27 18:42:43 +0200

    Installed and configured eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-﻿import React from "react";
-import ReactDOM from "react-dom/client";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>My wishlist with modern React</>
   </React.StrictMode>
