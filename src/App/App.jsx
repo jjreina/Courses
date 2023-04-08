03eec2eb8a7d94444fa8c07eef681764e0e9b196
jjreina <jreinapascual@gmail.com>
2023-04-08 21:46:00 +0200

    First version of Wishlist app

---
+++ src/App/App.jsx
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+
+const wishes = [
+  { name: 'Travel to the monn', done: false },
+  { name: 'Pay the gym', done: true },
+  { name: 'Go to the gym', done: false }
+];
+
+const App = () => (
+  <>
+    <h1>My wishlist with modern React</h1>
+    <fieldset>
+      <legend>New wish</legend>
+      <input type="text" placeholder="Enter your wish here" />
+    </fieldset>
+    <ul>
+      {wishes.map(({ name, done }) => (
+        <li key={name}>
+          <input type="checkbox" checked={done} />
+          <label>{name}</label>
+        </li>
+      ))}
+    </ul>
+    <button type="button">Archive done</button>
+  </>
+);
+
+export default App;
--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom/client';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <>My wishlist with modern React</>
-  </React.StrictMode>
-);
