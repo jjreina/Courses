cb26c7153a8757c876d305d752855701e476e4a9
jjreina <jreinapascual@gmail.com>
2023-04-14 21:13:49 +0200

    Added functionality to the WishItem component

--- src/App/WishList/WishItem.jsx
+++ src/App/WishList/WishItem.jsx
@@ -1,12 +1,36 @@
-﻿import React from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const WishItem = ({ name, done, onDoneChange }) => (
-  <li key={name} className={`wish-list__item ${done ? 'wish-list__item--done' : ''}`}>
-    <input type="checkbox" checked={done} onChange={(e) => onDoneChange(e.target.checked)} />
-    <label>{name}</label>
-  </li>
-);
+const WishItem = ({ name, done, onDoneChange }) => {
+  const [age, setAge] = useState(0);
+  useEffect(() => {
+    let ageInterval;
+    if (done) {
+      setAge(0);
+    } else {
+      ageInterval = setInterval(() => {
+        if (done) {
+          clearInterval(ageInterval);
+        } else {
+          setAge((age) => age + 1);
+        }
+      }, 1000);
+    }
+    return () => clearInterval(ageInterval);
+  }, [done]);
+  return (
+    <li
+      key={name}
+      className={`
+      wish-list__item ${done ? 'wish-list__item--done' : ''} 
+      wish-list__item ${age >= 3 ? 'wish-list__item--warning' : ''}
+      wish-list__item ${age >= 6 ? 'wish-list__item--danger' : ''}
+    `}>
+      <input type="checkbox" checked={done} onChange={(e) => onDoneChange(e.target.checked)} />
+      <label>{name}</label>
+    </li>
+  );
+};
 
 WishItem.propTypes = {
   name: PropTypes.string,
